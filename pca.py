bb14bcd7b900cac8ab737eb23deda6eccb957444
zztop <zaryab.ahmad@gmail.com>
2018-02-25 20:07:55 -0500

    full checkin

--- pca.py
+++ pca.py
@@ -1,13 +1,8 @@
-import pandas as pd
-from numpy.random import random
-import random
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import manifold, datasets
+import pandas as pd
+from sklearn import manifold
 from sklearn.decomposition import PCA
-
-from sklearn.preprocessing import scale
-from sklearn.metrics import pairwise_distances
 
 real_label = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -20,8 +15,9 @@ def unpickle(file):
 
 
 def mean_distance(class_data):
-    mds = manifold.MDS(2, max_iter=10)
-    return mds.fit_transform(class_data)
+    seed = np.random.RandomState(seed=3)
+    mds = manifold.MDS(2, max_iter=10, random_state=seed, dissimilarity='euclidean')
+    return mds.fit(class_data).dissimilarity_matrix_
 
 
 def show_mean_distance(label_mean_distance, fig_name):
@@ -35,17 +31,12 @@ def show_mean_distance(label_mean_distance, fig_name):
     plt.close()
 
 
-def compute_euclidean_distance(label_mean_distance):
-    D = pairwise_distances(label_mean_distance)
-    D.shape
-
-
 def create_error_bar(label_error_frame):
     plt.bar(real_label, label_error_frame, align='center', alpha=0.5)
     plt.ylabel('error')
     plt.title('Programming language usage')
 
-    plt.savefig('error_1')
+    plt.savefig('error_1.png')
     plt.close()
 
 
@@ -65,11 +56,11 @@ def part_3(all_labels, grp_dataset):
                     0])
         mixed_label_errors.append(mixed_class_label_error)
 
-    mean_mixed_label_errors=[]
+    mean_mixed_label_errors = []
     for idx, lbl in enumerate(all_labels):
-        mean_mixed_label_error=[]
-        for inner_idx,inner_lbl in enumerate(all_labels):
-            mean_mixed_label_error.append((mixed_label_errors[idx][inner_idx] + mixed_label_errors[inner_idx][idx])/2)
+        mean_mixed_label_error = []
+        for inner_idx, inner_lbl in enumerate(all_labels):
+            mean_mixed_label_error.append((mixed_label_errors[idx][inner_idx] + mixed_label_errors[inner_idx][idx]) / 2)
         mean_mixed_label_errors.append(mean_mixed_label_error)
 
     show_mean_distance(np.asarray(mean_mixed_label_errors), 'part_3_2d.png')
@@ -81,13 +72,12 @@ if __name__ == "__main__":
     all_data_array = None
     all_labels = None
 
-    # for file in ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5']:
-    for file in ['data_batch_1']:
+    for file in ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']:
         batch = unpickle(file)
         if all_data_array is None:
             all_data_array = batch[b'data']
         else:
-            np.concatenate((all_data_array, batch[b'data']))
+            all_data_array = np.vstack((all_data_array, batch[b'data']))
 
     # all_data_array = all_data_array.reshape(all_data_array.shape[0], 3, 32, 32)
     # labels = np.asarray(batch[b'labels'])
@@ -107,20 +97,24 @@ if __name__ == "__main__":
     label_error = []
     label_pca = []
 
-    part_3(labels, grouped_dataset)
-
     for i, label in enumerate(labels):
         g = grouped_dataset.get_group(label)
         g = g.drop(g.columns[[3072]], axis=1)
 
-        # Part 2 For 10 * 10 matrix  and MDS
+# ****************Error Part 1
+        pca = PCA(n_components=20)
+        pca.fit(g)
+        # total_error = np.sum(pca.explained_variance_ratio_)
+        label_error.append(((pca.inverse_transform(pca.transform(g)) - g).pow(2)).sum().sum() / g.shape[0])
+
+# *************** Part 2 For 10 * 10 matrix  and MDS
         mean_image = g.agg([np.mean])
         # # wrong label_frame[label] = mean_distance(g)
         if label_mean_frame is None:
             label_mean_frame = pd.DataFrame(mean_image)
         else:
             label_mean_frame = label_mean_frame.append(mean_image)
-
+# *****************************************************
 
 
             # Show Images
@@ -132,40 +126,20 @@ if __name__ == "__main__":
             #         axes1[j][k].set_axis_off()
             #         axes1[j][k].imshow(reshaped_g[i:i + 1][0])
 
-            # Error Part 1
-            # pca = PCA(n_components=20)
-            # pca.fit(g)
-            # # total_error = np.sum(pca.explained_variance_ratio_)
-            # label_error.append(((pca.inverse_transform(pca.transform(g)) - g).pow(2)).sum().sum() / g.shape[0])
+
+# ***************For Error Part 1
+    create_error_bar(label_error)
+
+# ************ For Part 2
+    label_mean_mds = mean_distance(label_mean_frame)
+    show_mean_distance(label_mean_mds, 'part_2_2d')
+
+# *************** PART -3************
+    part_3(labels, grouped_dataset)
+# ***************End Of  PART -3************
 
 
 
 
-
-
-
-
-            #
-            # reshaped_g = np.transpose(np.reshape(filtered, (g.shape[0], 3, 32, 32)), (0, 2, 3, 1))
-            # fig, axes1 = plt.subplots(5, 5, figsize=(3, 3))
-            # for j in range(5):
-            #     for k in range(5):
-            #         i = np.random.choice(range(len(reshaped_g)))
-            #         axes1[j][k].set_axis_off()
-            #         axes1[j][k].imshow(reshaped_g[i:i + 1][0])
-
-# plt.imshow(np.transpose(mean_image.iloc[0:1].values.reshape(3, 32, 32), (1, 2, 0)))
-
-
-
-
-
-# For Part 2
-# label_mean_mds = mean_distance(label_mean_frame)
-# show_mean_distance(label_mean_mds, 'part_2_2d')
-# compute_euclidean_distance(label_mean_frame)
-
-# For Error Part 1
-# create_error_bar(label_error)
 
 print('done')
